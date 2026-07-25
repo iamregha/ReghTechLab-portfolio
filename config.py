b65a715fa5ae08e57cecacfc9da65e88291ed118
Abraham <iamregha@gmail.com>
2026-07-25 23:40:21 +0100

    fix: Edit config for email verification

--- config.py
+++ config.py
@@ -32,12 +32,14 @@ class Config:
     CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
 
     # Mail Configuration
-    MAIL_SERVER       = os.environ.get("MAIL_SERVER", "localhost")
-    MAIL_PORT         = int(os.environ.get("MAIL_PORT", 25))
-    MAIL_USE_TLS      = os.environ.get("MAIL_USE_TLS", "False").lower() in ["true", "1"]
+    MAIL_SERVER       = os.environ.get("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_PORT         = int(os.environ.get("MAIL_PORT", 587))
+    MAIL_USE_TLS      = os.environ.get("MAIL_USE_TLS", "True").lower() in ["true", "1", "t"]
+    MAIL_USE_SSL = os.environ.get("MAIL_USE_SSL", "False").lower() in ["true", "1", "t"]
     MAIL_USERNAME     = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD     = os.environ.get("MAIL_PASSWORD")
     MAIL_DEFAULT_SENDER = os.environ.get("MAIL_DEFAULT_SENDER", "noreply@reghtechlab.com")
+    MAIL_SUPPRESS_SEND = False
     CONTACT_EMAIL     = os.environ.get("CONTACT_EMAIL", "regha87@gmail.com")
 
     # Caching — SimpleCache = in-process memory, zero extra infra needed.
