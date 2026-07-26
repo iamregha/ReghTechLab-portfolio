74f2bd3eaf1ec48db915fd9d7418206987d95274
Abraham <iamregha@gmail.com>
2026-07-26 01:38:09 +0100

    fix: configured resend

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -17,7 +17,7 @@ import os
 import cloudinary
 from flask_migrate import upgrade as flask_upgrade
 
-from .extensions import db, migrate, login_manager, csrf, limiter, mail, cache
+from .extensions import db, migrate, login_manager, csrf, limiter, cache
 from config import config
 
 
@@ -42,7 +42,7 @@ def create_app(config_name: str = None) -> Flask:
     login_manager.login_message_category = 'info'
     csrf.init_app(app)
     limiter.init_app(app)
-    # mail.init_app(app)
+    
     cache.init_app(app)
 
     from portfolio import models
